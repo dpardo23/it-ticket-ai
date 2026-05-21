667972b8034d3990ee218b6fc22660bb21d2720a
dpardo <jdiegopardo23@gmail.com>
2026-05-21 11:44:11 -0400

    feat: interfaz borderless con decoraciones personalizadas

---
+++ ui/src-tauri/src/lib.rs
@@ -0,0 +1,28 @@
+use tauri::Manager;
+
+#[tauri::command]
+async fn show_main_window(window: tauri::Window) {
+    if let Some(main_window) = window.get_webview_window("main") {
+        main_window.show().unwrap();
+    }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![show_main_window])
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
